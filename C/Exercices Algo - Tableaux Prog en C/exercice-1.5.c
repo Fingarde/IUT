44d2f0c99d2820a1956e92bae8d161c7243cff68
Fingarde <fingarde@gmail.com>
2019-09-09 17:30:29 +0200

    Added exercice 1.5

--- C/Exercices Algo - Tableaux Prog en C/exercice-1.5.c
+++ C/Exercices Algo - Tableaux Prog en C/exercice-1.5.c
@@ -6,9 +6,7 @@ finalité: Donner le prix qu'un client devra payer pour un nombre de nuits
 		et de petits dejeuners défini par l'opérateur.
 */
 
-
 #include <stdio.h>
-
 
 int main() {
 	calculerPrix();
@@ -21,8 +19,8 @@ int main() {
 	variables: 
 		nbNuit			Nombre de nuits qu a dormi le client
 		nbDejeuner		Nombre de dejeuners qu a pris le client
-		totalNuit		Prix pour le nombre de nuits ( 1 nuit = 50€)
-		totalDejeuner	Prix pour le nombre de dejeuners ( 1 dejeuner = 5€)
+		totalNuit		Prix pour le nombre de nuits ( 1 nuit = 50€ )
+		totalDejeuner	Prix pour le nombre de dejeuners ( 1 dejeuner = 5€ )
 		totalSansTva	Prix total sans la tva de 19.6%
 		tva				Montant de la tva de 19.6% tu le prix total
 
@@ -37,10 +35,10 @@ void calculerPrix()
 	printf("Nombre de dejeuners: \n");
 	scanf("%f", &nbDejeuner);
 
-	totalNuit = nbNuit * 50;
-	totalDejeuner = nbDejeuner * 5;
-	totalSansTva = totalNuit + totalDejeuner;
-	tva = totalSansTva * 0.196;
+	totalNuit = nbNuit * 50; // Calcul du montant des nuits ( 1 nuit correspond a 50€ )
+	totalDejeuner = nbDejeuner * 5; // Calcul du montant des dejeuners ( 1 dejeuner correspond a 50€ )
+	totalSansTva = totalNuit + totalDejeuner; // Calcul du total
+	tva = totalSansTva * 0.196; // Calcul de la valeur de la taxe a 19.6%
 
 	printf("Montant TTC pour la chambre: %.2f\n", totalNuit);
 	printf("Montant TTC pour les petits dejeuners: %.2f\n", totalDejeuner);

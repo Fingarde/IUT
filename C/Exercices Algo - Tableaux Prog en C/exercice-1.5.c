2307de798ccc46c201a28e84c9b585d3ce7989f2
Fingarde <fingarde@gmail.com>
2019-09-09 18:04:19 +0200

    Added exercice 1.5

--- C/Exercices Algo - Tableaux Prog en C/exercice-1.5.c
+++ C/Exercices Algo - Tableaux Prog en C/exercice-1.5.c
@@ -1,18 +1,13 @@
 /*
-		programme: exercice-1.5.c
-		auteur: Bertrand Timothé
-		date: 09/09/2019
-		finalité: Donner le prix qu'un client devra payer pour un nombre de nuits
-				  et de petits dejeuners défini par l'opérateur.
+programme: exercice-1.5.c
+auteur: Bertrand Timothé
+date: 09/09/2019
+finalité: Donner le prix qu'un client devra payer pour un nombre de nuits
+		et de petits dejeuners défini par l'opérateur.
 */
 
+
 #include <stdio.h>
-
-int main() {
-	calculerPrix();
-
-	return 0;
-}
 
 /*
 	nom: calculerPrix
@@ -21,30 +16,34 @@ int main() {
 	variables: 
 		nbNuit			Nombre de nuits qu a dormi le client
 		nbDejeuner		Nombre de dejeuners qu a pris le client
-		totalNuit		Prix pour le nombre de nuits ( 1 nuit = 50€ )
-		totalDejeuner	Prix pour le nombre de dejeuners ( 1 dejeuner = 5€ )
+		totalNuit		Prix pour le nombre de nuits ( 1 nuit = 50€)
+		totalDejeuner	Prix pour le nombre de dejeuners ( 1 dejeuner = 5€)
 		totalSansTva	Prix total sans la tva de 19.6%
 		tva				Montant de la tva de 19.6% tu le prix total
 
 */
 void calculerPrix()
 {
-	int nbNuit, nbDejeuner;
-	float totalNuit, totalDejeuner, totalSansTva, tva;
+	float nbNuit, nbDejeuner, totalNuit, totalDejeuner, totalSansTva, tva;
 
 	printf("Nombre de nuits: \n");
-	scanf("%d", &nbNuit);
+	scanf("%f", &nbNuit);
 
 	printf("Nombre de dejeuners: \n");
-	scanf("%d", &nbDejeuner);
+	scanf("%f", &nbDejeuner);
 
-	totalNuit = nbNuit * 50; // Calcul du montant des nuits ( 1 nuit correspond a 50€ )
-	totalDejeuner = nbDejeuner * 5; // Calcul du montant des dejeuners ( 1 dejeuner correspond a 50€ )
-	totalSansTva = totalNuit + totalDejeuner; // Calcul du total
-	tva = totalSansTva * 0.196; // Calcul de la valeur de la taxe a 19.6%
+	totalNuit = nbNuit * 50;
+	totalDejeuner = nbDejeuner * 5;
+	totalSansTva = totalNuit + totalDejeuner;
+	tva = totalSansTva * 0.196;
 
 	printf("Montant TTC pour la chambre: %.2f\n", totalNuit);
 	printf("Montant TTC pour les petits dejeuners: %.2f\n", totalDejeuner);
 	printf("Total a payer: %.2f\n", totalSansTva);
-	printf("Montant de la TVA (19.6%%): %.2f\n", tva);
+	printf("Montant de la TVA (19.6%%1): %.2f\n", tva);
+}
+
+
+int main() {
+	calculerPrix();
 }
